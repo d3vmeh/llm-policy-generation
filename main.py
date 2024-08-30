41b67fb7a27fb14c6492888f97b51cdba6a8d45f
d3vmeh <devxmehra@gmail.com>
2024-08-29 23:59:24 -0700

    Streamlit UI testing

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from querying import *
 import sys
+import streamlit as st
 
+# print("Loaded all")
+# i = 0
+# question = st.text_input("Enter a prompt",key=str(i))
+# is_clicked = st.button("Submit")
 
+# while True:
+        
+    
+#     # print(type(question))
+#     # if question.lower() == "exit" or question.lower() == "q":
+#     #     break
+#     if is_clicked:
+#         response = chain.invoke(question)
+#         print(type(response))
+#         st.write(response)
+#         i += 1
+#         is_clicked = False
 while True:
     q = input("\nEnter a query: ")
     if q.lower() == "exit" or q.lower() == "q":
